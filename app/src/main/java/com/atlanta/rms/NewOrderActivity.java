285ec8d06cbf66c9dace325bc0d24136a835337e
cybrix <cybrixg@gmail.com>
2021-10-06 13:26:06 +0530

    new order stage 5

--- app/src/main/java/com/atlanta/rms/NewOrderActivity.java
+++ app/src/main/java/com/atlanta/rms/NewOrderActivity.java
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.builders.DatePickerBuilder;
+import com.applandeo.materialcalendarview.listeners.OnSelectDateListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class NewOrderActivity extends AppCompatActivity {
 
 
     TextView txtparty,txtinvno,dtdate,txtsalestype,txtbillamount;
     TextView lblmobilenumber;
-    Button btnAddItem;
+    Button btnAddItem,btnCalandar;
     Boolean blnNewRecord=false;
+    Calendar cal = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +36,7 @@ public class NewOrderActivity extends AppCompatActivity {
         txtbillamount=findViewById(R.id.txtbillamount);
         lblmobilenumber=findViewById(R.id.lblnewordermobileno);
         btnAddItem=findViewById(R.id.btnadditem);
+        btnCalandar=findViewById(R.id.btnCalandar);
         final Intent intent = getIntent();
         Bundle bd = intent.getExtras();
         txtparty.setText((String) bd.get("PartyName"));
@@ -32,8 +44,35 @@ public class NewOrderActivity extends AppCompatActivity {
         txtsalestype.setText(Common.sCurrentOrderType);
         lblmobilenumber.setText((String) bd.get("MobileNumber"));
         blnNewRecord=(Boolean) bd.get("NewRecord");
+        dtdate.setText(new SimpleDateFormat("dd-MM-yyyy").format(new Date()));
+
+        btnCalandar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
+                OnSelectDateListener listener = new OnSelectDateListener() {
+                    @Override
+                    public void onSelect(final List<Calendar> calendars) {
+
+                        Calendar calendar = Calendar.getInstance();
+                        if (calendar.compareTo(calendars.get(0)) <= 0) {
+                            cal = calendars.get(0);
+                            dtdate.setText(new SimpleDateFormat("dd-MM-yyyy").format(calendars.get(0).getTime()));
+
+                        } else {
+                            dtdate.setText("Select Date");
+
+                        }
+                    }
+                };
+                DatePickerBuilder builder = new DatePickerBuilder(NewOrderActivity.this, listener)
+                        .pickerType(CalendarView.ONE_DAY_PICKER).date(cal);
+
+                com.applandeo.materialcalendarview.DatePicker datePicker = builder.build();
+                datePicker.show();
+            }
+        });
 
     }
 }
